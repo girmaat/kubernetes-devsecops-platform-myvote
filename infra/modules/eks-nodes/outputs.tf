1adad8aa3830b2185595eaca2c18dc2f57fcc1e7
girmaat <girmaat@gmail.com>
2025-06-14 11:02:13 +0000

    Refactored: Cleanup infra/ layout and remove obsolete modules

--- infra/modules/eks-nodes/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "node_group_name" {
-  value = aws_eks_node_group.this.node_group_name
-}
-
-output "node_role_arn" {
-  value = aws_iam_role.node_role.arn
-}
--- infra/modules/eks/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cluster_name" {
-  type        = string
-  description = "Name of the EKS cluster"
-}
-
-variable "subnet_ids" {
-  type        = list(string)
-  description = "Subnets to launch the EKS control plane into"
-}
